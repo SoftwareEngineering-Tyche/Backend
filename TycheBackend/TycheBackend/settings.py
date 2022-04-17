08a5651f09a0715740252a46364ed2b124da7a93
HasanSabourIUST <hasan.sabour.iust@gmail.com>
2022-04-17 14:11:02 +0430

    update settings.py

--- TycheBackend/TycheBackend/settings.py
+++ TycheBackend/TycheBackend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "corsheaders",
+    'corsheaders',
     'drf_yasg',
     'rest_framework_swagger',
     'Account',

8feb7e66ea380f61f2bfe4ea0b6d8057591f6bcd
HasanSabourIUST <hasan.sabour.iust@gmail.com>
2022-04-21 14:31:07 +0430

    update Account Test

--- TycheBackend/Account/tests.py
+++ TycheBackend/Account/tests.py
@@ -10,10 +10,11 @@ class AccountTestCase(APITestCase):
         Account.email = 'Hasan@gmail.com'
         Account.WalletInfo="dvvdfivkl"
         #account.objects.create('Accfdsfnt','fdsfsdf',null=True,null=True,null=True,null=True,null=True,null=True,'sdfsfscsdjkv')
-        url = 'http://127.0.0.1:8000/account/'
-        data = {'Name': 'hasan.sabour.iust@gmail.com',
-                'Description':'hasan'}
+        url = 'http://127.0.0.1:8000/Account/'
+        data = {'WalletInfo': 'hasan.sabour.iust@gmail.com',
+                'username':'hasan'}
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        #self.assertEqual(response.data, data)
+        self.assertEqual(response.data.get('username'), data.get('username'))
+        self.assertEqual(response.data.get('WalletInfo'), data.get('WalletInfo'))
         #self.assertIsInstance(Account,account)

a4145b2e4abc813d146e5c13e8ea1663b867a37a
HasanSabourIUST <hasan.sabour.iust@gmail.com>
2022-04-21 13:07:53 +0430

    Update Collection models

--- TycheBackend/Account/models.py
+++ TycheBackend/Account/models.py
@@ -19,20 +19,21 @@ class WorkArt(models.Model):
     Externallink=models.URLField(null=True,blank=True)
     Description=models.CharField(max_length=200,null=True,blank=True)
     Supply=models.IntegerField(null=True,blank=True)
+    Liked=models.IntegerField(null=True,blank=True)
+    
     #BlockChain=models.CharField(max_length=5,null=True,blank=True)
     
 class collection(models.Model):
-    Logoimage=models.ImageField(_("Image"),upload_to=upload_to, null=True, blank=True)
-    Featuredimage=models.ImageField(_("Image"),upload_to=upload_to, null=True, blank=True)
-    Bannerimage=models.ImageField(_("Image"),upload_to=upload_to, null=True, blank=True)
+    Logoimage=models.ImageField(upload_to=upload_to, null=True, blank=True)
+    Featuredimage=models.ImageField(upload_to=upload_to, null=True, blank=True)
+    Bannerimage=models.ImageField(upload_to=upload_to, null=True, blank=True)
     Name=models.CharField(max_length=200,null=True,blank=True)
     URL=models.CharField(max_length=200,null=True,blank=True)
     Description=models.CharField(max_length=900,null=True,blank=True)
     category=models.CharField(max_length=200,null=True,blank=True)
     DisplayTheme=models.CharField(max_length=200,null=True,blank=True)
     WorkArts=models.ManyToManyField(WorkArt,related_name='collections', verbose_name=_('WorkArts'),null=True,blank=True)
-
-
+    
 class account(models.Model):
     username=models.CharField(max_length=100,null=True,blank=True)
     bio=models.CharField(max_length=500,null=True,blank=True)
@@ -43,6 +44,5 @@ class account(models.Model):
     favorite=models.ManyToManyField(WorkArt,related_name='favorites', verbose_name=_('members'),null=True,blank=True)
     collection=models.ManyToManyField(collection,verbose_name=_('members'),related_name='collections',null=True, blank=True)
     WalletInfo=models.CharField(max_length=500, primary_key=True)
-    
     
 #class WalletInfo(models.Model):
--- TycheBackend/Account/views.py
+++ TycheBackend/Account/views.py
@@ -11,11 +11,20 @@ from rest_framework.parsers import MultiPartParser,FormParser
 class Account(APIView):
     def post(self,request):
         parser_classes=[MultiPartParser, FormParser]
-        serializer=AccountSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_201_CREATED)
+        #serializer=AccountSerializer(data=request.data)
+        #if serializer.is_valid():
+        #    serializer.save()
+        #    return Response(serializer.data, status=status.HTTP_201_CREATED)
+        query=Account()
+        query.WalletInfo=request.data['WalletInfo']
+        query.username=request.data['username']
+        query.avatar=request.data['avatar']
+        query.WalletInfo=request.data['WalletInfo']
+        query.banner=request.data['banner']
+        query.bio=request.data['bio']
+        query.email=request.data['email']
+        query.save()
+        return Response(request.data, status=status.HTTP_201_CREATED)
     def put(self,request,format=None):
         parser_classes=(MultiPartParser, FormParser)
         try:
@@ -51,18 +60,40 @@ class collectionView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        #query=collection()
+        #query.Logoimage=request.data['Logoimage']
+        #query.Featuredimage=request.data['Featuredimage']
+        #query.Bannerimage=request.data['Bannerimage']
+        #query.Name=request.data['Name']
+        #query.URL=request.data['URL']
+        #query.Description=request.data['Description']
+        #query.category=request.data['category']
+        #query.DisplayTheme=request.data['DisplayTheme']
+        #query.WorkArts=request.data['WorkArts']
+        #query.save()
+        
+        return Response(request.data, status=status.HTTP_201_CREATED)
     def put(self,request,pk):
         parser_classes=[MultiPartParser, FormParser]
         try:
             query=collection.objects.get(id=pk)
         except:
             return Response("Error",status.HTTP_400_BAD_REQUEST)
+        #query.Logoimage=request.data['Logoimage']
+        #query.Featuredimage=request.data['Featuredimage']
+        #query.Bannerimage=request.data['Bannerimage']
+        #query.Name=request.data['Name']
+        #query.URL=request.data['URL']
+        #query.Description=request.data['Description']
+        #query.category=request.data['category']
+        #query.DisplayTheme=request.data['DisplayTheme']
+        #query.WorkArts=request.data['WorkArts']
+        #query.save()
         serializer=CollectionSerializer(query,data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status.HTTP_200_OK)
-        return Response(serializer.data,status.HTTP_400_BAD_REQUEST)
+        return Response(query.data,status.HTTP_200_OK)
     def get(self,request,pk):
         query=collection.objects.get(id=pk)
         serializer=CollectionSerializer(query)
